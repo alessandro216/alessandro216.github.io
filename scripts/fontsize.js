6c3a2b514af6a939d27befcb16d901cf0ef9d344
Alessandro <58531323+alessandro216@users.noreply.github.com>
2022-04-11 00:15:32 +0200

    Delete scripts directory

--- scripts/fontsize.js
+++
@@ -1,6 +0,0 @@
-function resize (factor) {
-  let sizeStr = document.body.style.fontSize;
-  let size = Number(sizeStr.replace('em', '')) || 1.2;
-  let bigger = (size * factor).toFixed(1) + 'em';
-  document.body.style.fontSize = bigger;
-}
